15e9c1fe927eec10a8ad29eaecf5d32c7672e908
SasquatchShawn <srmiranda1@gmail.com>
2015-12-14 21:26:57 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', function($scope) {
+    
+    $scope.alexWork = {
+            "cID": "-1",
+            "eID": "1441",
+            "evYear": "2015",
+            "qualifier": null,
+            "evName": "CDGA Senior Better Ball of Pairs Qualifier #2",
+            "sDate": 1433134800000,
+            "eDate": 1433134800000,
+            "teamSize": "2",
+            "datedisp": "Jun 01, 2015",
+            "etid": "1",
+            "clAbbrev": "Elgin CC",
+            "past": "1",
+            "scoringType": "4",
+            "clubHometown": "C",
+            "doCut": "1",
+            "doAlt": "1",
+            "cutRound": "0",
+            "cutTopX": 0,
+            "includeTies": "0",
+            "tenFromLead": "0",
+            "numStrokePlayRounds": 1,
+            "hostClubID": "110",
+            "courseID": "111",
+            "useFlights": "1",
+            "byUser": "APD",
+            "lastUpdate": "2015-06-23T18:37:00",
+            "inLocal": "1",
+            "inPlayers": "1",
+            "inFlights": "0",
+            "inTeeSheets": "1",
+            "inPairings": "1",
+            "handicapPercent": "100",
+            "dotCards": "0",
+            "maxDiff": 4.4,
+            "logoid": "1",
+            "bLive": "1",
+            "lsRefreshRate": "0",
+            "lsMessage": "<br>6 Teams in each flight will be invited to the Senior Better Ball Championship on Monday, June 29 at McHenry CC. (5 gross teams and 1 net team) <br>The Net Team that advances is the lowest score in the flight that has not advanced in the Gross.<br><br> For all ties in Gross and Net flights, the committee will use the USGA recommendation for breaking ties under Rule 33-6 which is defined by holes 10-18.",
+            "lsRoundNum": "1",
+            "treeSize": 0,
+            "combined": "N",
+            "rounds": [
+              {
+                  "roundNum": "1",
+                  "roundDate": 1433134800000,
+                  "scoringType": 4,
+                  "mpRound": "-1",
+                  "cutApplied": "1"
+              }
+            ]
+        };
+    
+}]);
